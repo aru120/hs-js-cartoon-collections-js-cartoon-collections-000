1400cd313d179a947d26c1dc56b052ce92123e12
Kevin Huang <khuang159@gmail.com>
2020-09-07 18:10:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 function dwarfRollCall(dwarves) {
+  var str = "";
+  for (var i=0; i<dwarves.length; i++){
+    str += (i+1)+". "+ dwarves[i]+" "; 
+  }
+  return str;
 }
 
 function summonCaptainPlanet(planeteerCalls){
-}
+  const capital = [];
+for (var i = 0; i<planeteerCalls.length; i++){
+    capital[i] = planeteerCalls[i].toUpperCase() +"!";
+  }
+  return capital;
+}   
 
 function longPlaneteerCalls(words) {
+  for (var i = 0; i<words.length; i++){
+    if(words[i].length > 4){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
 }
 
 function findTheCheese (foods) {
+  let cheese = ["cheddar","gouda","camembert"];
+  for (var i = 0; i<foods.length;i++){
+    for (var j = 0; j<cheese.length; j++){
+      if (foods[i] === cheese[j]){
+        return foods[i];
+      }
+    }
+  }
+    return "no cheese!";
 }
